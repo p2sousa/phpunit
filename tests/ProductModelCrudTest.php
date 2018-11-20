b117bc02c626d36fc19c20a1ac8370bb02c680af
Pablo Sousa <pablosousa.ads@gmail.com>
2018-11-19 22:25:21 -0200

    Tests: ProductModelCrud done tests save and list all products.

--- tests/ProductModelCrudTest.php
+++ tests/ProductModelCrudTest.php
@@ -21,6 +21,25 @@ class ProductModelCrudTest extends TestCase
             "quantity" => 2
         ]);
         
-        print_r($result);
+        $this->assertEquals(1, $product->getId());
+        $this->assertEquals('Smartphone', $product->getName());
+        $this->assertEquals(1500, $product->getPrice());
+        $this->assertEquals(2, $product->getQuantity());
+        $this->assertEquals(1500*2, $product->getTotal());
+    }
+    
+    public function testIfListProducts()
+    {
+        global $db;
+        
+        $product = new ProductModel($db);
+        $result = $product->save([
+            "name" => "Smartphone",
+            "price" => 1500.00,
+            "quantity" => 2
+        ]);
+        
+        $products = $product->all();
+        $this->assertCount(2, $products);
     }
 }
