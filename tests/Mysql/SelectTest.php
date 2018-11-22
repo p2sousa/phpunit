6c3fc0ebd12066334831058393892f7435a7cf26
Pablo Sousa <pablosousa.ads@gmail.com>
2018-11-22 00:16:40 -0200

    TDD: QueryBuilder class Select, stage Red

---
+++ tests/Mysql/SelectTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Query\Mysql;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Description of SelectTest
+ *
+ * @author pablosousa <pablosousa.ads@gmail.com>
+ */
+class SelectTest extends TestCase
+{
+    public function testWithoutFilter()
+    {
+        $select = new Select;
+        $select->setTable('products');
+        
+        $this->assertEquals(
+            'SELECT * FROM products', 
+            $select->getSql()
+        );
+    }
+}
